f0cd81b00214944f9bf59ebc5b415a999ef33e5f
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-09-05 17:45:59 -0300

    GetAccountHistory method added.

--- Program.cs
+++ Program.cs
@@ -7,12 +7,39 @@ namespace classes
     {
         static void Main(string[] args)
         {
-            var account = new BankAccount("Jonatas", 1250);
+            var account = new BankAccount("Jonatas", 10000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
 
             account.MakeWithdrawal(120, DateTime.Now, "Hammock");
 
             Console.WriteLine(account.Balance);
+
+            // Test that the initial balances must be positive.
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+
+            // Test for a negative balance.
+            try
+            {
+                account.MakeWithdrawal(75000, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+
+            account.MakeWithdrawal(500, DateTime.Now, "Xbox Game");
+
+            Console.WriteLine(account.GetAccountHistory());
+
         }
     }
 }
