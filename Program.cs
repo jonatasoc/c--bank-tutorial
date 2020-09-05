6ef9a9ecc8c71c135ca0891d18992afd0938ec9e
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-09-05 00:11:10 -0300

    Initial Commit of Bank Account

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Jonatas", 1250);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+        }
+    }
+}
